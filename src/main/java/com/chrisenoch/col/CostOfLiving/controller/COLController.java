88e7f4e46200dd4c85f232a83f148353e9c1c291
chrisenoch <45094010+chrisenoch@users.noreply.github.com>
2020-10-14 17:36:19 +0200

    @RestController added in place of @Controller for clarity

--- src/main/java/com/chrisenoch/col/CostOfLiving/controller/COLController.java
+++ src/main/java/com/chrisenoch/col/CostOfLiving/controller/COLController.java
@@ -7,17 +7,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.chrisenoch.col.CostOfLiving.entity.COLIndex;
 import com.chrisenoch.col.CostOfLiving.entity.COLIndexes;
 import com.chrisenoch.col.CostOfLiving.entity.COLResults;
 import com.chrisenoch.col.CostOfLiving.service.CostOfLivingService;
 
-@Controller
+@RestController
 @RequestMapping("/costofliving")
 public class COLController {
 	
