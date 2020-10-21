8cefaf6d0ea85d5c4e36796b3d09b4253cd5ffcd
chrisenoch <45094010+chrisenoch@users.noreply.github.com>
2020-10-21 20:34:03 +0200

    exception handlers added

---
+++ src/main/java/com/chrisenoch/col/CostOfLiving/error/COLIndexNotFoundException.java
@@ -0,0 +1,22 @@
+package com.chrisenoch.col.CostOfLiving.error;
+
+public class COLIndexNotFoundException extends RuntimeException {
+
+
+	public COLIndexNotFoundException(String message, Throwable cause) {
+		super(message, cause);
+
+	}
+
+	public COLIndexNotFoundException(String message) {
+		super(message);
+
+	}
+
+	public COLIndexNotFoundException(Throwable cause) {
+		super(cause);
+
+	}
+	
+	
+}
